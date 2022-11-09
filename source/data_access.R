d7a1da789b857c8863c280b55d223ec8d9de9e88
xinmew <114700698+xinmew@users.noreply.github.com>
2022-11-08 16:31:51 -0800

    trimming data 11/8

--- source/data_access.R
+++ source/data_access.R
@@ -1,8 +1,24 @@
-# Example: Project Data Access Functions  ----
-#----------------------------------------------------------------------------#
-# These functions are used to access data sources ... 
-#----------------------------------------------------------------------------#
-data_access_test <- function (name="World!") {
-  t <- paste0("Hello: ", name)
-  return(t)
+library(readr)
+
+# pls make sure to run all of this when loading data
+Shanghai <- read.csv("data/Shanghai_PM2.5_2013-2021.csv")
+Shanghai <- subset(Shanghai, Site != 'Site')
+Shenyang <- read.csv("data/Shenyang_PM2.5_2013-2021.csv")
+Shenyang <- subset(Shenyang, Site != 'Site')
+Guangzhou <- read.csv("data/Guangzhou_PM2.5_2013-2021.csv")
+Guangzhou <- subset(Guangzhou, Site != 'Site')
+Beijing <- read.csv("data/Beijing_PM2.5_2013-2021.csv")
+Beijing <- subset(Beijing, Site != 'Site')
+
+
+# work in progress to calculate the aveage aqi per day
+# pls feel free to change or add on to it
+# the issue is changing the date column to actual date objects
+for(i in 1:length(Shanghai$Date..LT.)){
+  Shanghai$Date..LT.[i] <- (substring(Shanghai$Date..LT.[i], 1,10))
 }
+
+Shanghai <- Shanghai %>%
+  group_by(as.Date(Date..LT.)) %>%
+  summarize(AQI=(sum(AQI)/24))
+
---
+++ source/summary.R
@@ -0,0 +1,12 @@
+source(data_access.R)
+
+library(dplyr)
+
+Shanghai <- read.csv("data/Shanghai_PM2.5_2013-2021.csv")
+Shenyang <- read.csv("data/Shenyang_PM2.5_2013-2021.csv")
+Guangzhou <- read.csv("data/Guangzhou_PM2.5_2013-2021.csv")
+Beijing <- read.csv("data/Beijing_PM2.5_2013-2021.csv")
+
+
+  
+  
