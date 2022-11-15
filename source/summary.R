c087a148f93052794d4606540ba8d7f7d6fb82e5
lyarrington <lyarri@uw.edu>
2022-11-14 16:36:19 -0800

    11/14 4:36 Lauren

--- source/summary.R
+++ source/summary.R
@@ -5,26 +5,11 @@ source(data_access.R)
 
 library(dplyr)
 
-<<<<<<< HEAD
 Shanghai <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-lyarrington/main/data/Shanghai_PM2.5_2013-2021.csv")
 Shenyang <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-lyarrington/main/data/Shenyang_PM2.5_2013-2021.csv")
 Guangzhou <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-lyarrington/main/data/Guangzhou_PM2.5_2013-2021.csv")
 Beijing <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-lyarrington/main/data/Beijing_PM2.5_2013-2021.csv")
-=======
-#
-# Can someone check if the read.csv line works on their computer. I don't think the path is right
-#
-# ^^ I have a temporary fix for this, seems like the code no longer works once u
-# pull it from github, pls make sure the run the code below:\
-setwd("C:/Users/simon/Documents/info201/assignments/project-lyarrington/")
-# adjust it to whatever your local path is to your project and that the 
-# the data below should load normally.
 
-Shanghai <- read.csv("data/Shanghai_PM2.5_2013-2021.csv")
-Shenyang <- read.csv("data/Shenyang_PM2.5_2013-2021.csv")
-Guangzhou <- read.csv("data/Guangzhou_PM2.5_2013-2021.csv")
-Beijing <- read.csv("data/Beijing_PM2.5_2013-2021.csv")
->>>>>>> 0d2fbb0f84fbe8d44e47bf6ac680c7ca502d509c
 
 my_df <- full_join(Shanghai, Shenyang)
 my_df <- full_join(my_df, Guangzhou)
